7fe679373416334c0d0c07127af0e88441b5ba41
fufdfiubfdsdf <152696798+fufdfiubfdsdf@users.noreply.github.com>
2025-05-18 15:58:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 def load_bot_configs():
-         return {
-             "bot2": {
-                 "TOKEN": "8173622705:AAE88BPX5k1mHuwFFBlWJS8ixxa36EmuCC0",
-                 "YOOMONEY_WALLET": "4100118178122985",
-                 "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
-                 "PRIVATE_CHANNEL_ID": -1002609563244,
-                 "PRICE": 3.00,
-                 "DESCRIPTION": (
-                     "Plan: Standard\n"
-                     "Cost: {price} 🇷🇺RUB\n"
-                     "Duration: 1 month\n\n"
-                     "Subscription to private channel"
-                 )
-             }
-         }
+    return {
+        "bot2": {
+            "TOKEN": "8173622705:AAE88BPX5k1mHuwFFBlWJS8ixxa36EmuCC0",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002609563244,
+            "PRICE": 3.00,
+            "DESCRIPTION": (
+                "Тариф: Стандарт\n"
+                "Стоимость: {price} 🇷🇺RUB\n"
+                "Срок действия: 1 месяц\n\n"
+                "Подписка на закрытый канал"
+            )
+        }
+    }
